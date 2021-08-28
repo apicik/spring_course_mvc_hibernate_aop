62890e7315429ce623923eada0eef34417032201
snake909 <44599182+snake909@users.noreply.github.com>
2021-08-29 00:51:58 +0400

    first commit

---
+++ src/main/java/com/pitsik/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,25 @@
+package com.pitsik.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    @Around("execution(* com.pitsik.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(
+            ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Execute method " + methodName);
+
+        return proceedingJoinPoint.proceed();
+    }
+
+}
